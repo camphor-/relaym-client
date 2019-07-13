1ef8a79a3b0f084435c5d8c53d1e201932424043
ataran <14242642+aratasato@users.noreply.github.com>
2019-07-13 23:57:20 +0900

    Toppage sepalate components (#9)
    * migrate add-place-card
    
    * コンポーネントに分けた
    
    * add click handler

---
+++ components/organisms/PlacesList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="places_list">
+    <v-card-title>
+      <span class="card_title">周りの場所</span>
+    </v-card-title>
+    <v-list>
+      <v-list-tile
+        v-for="(place, index) in places"
+        :key="place.name"
+        @click="1"
+      >
+        <v-list-tile-content>
+          <v-list-tile-title v-text="place.name"></v-list-tile-title>
+          <v-divider :key="index"></v-divider>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Place from '@/models/Place'
+
+@Component({
+  components: {}
+})
+export default class extends Vue {
+  @Prop({ default: [] }) readonly places!: Place[]
+}
+</script>
+<style lang="scss" scoped>
+.card_title {
+  font-size: 1.5rem;
+}
+.places_list {
+  padding: 10px;
+  .place_name {
+    font-size: 1.2rem;
+    padding: 10px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,47 +7,23 @@
         </a>
       </div>
 
-      <v-card class="new_place">
-        <v-card-title>
-          <span class="card_title">新しい場所を追加する</span>
-        </v-card-title>
-        <v-form>
-          <v-text-field label="place id"></v-text-field>
-          <v-text-field label="Name"></v-text-field>
-          <div class="text-xs-right">
-            <v-btn round color="#0D47A1" class="white_text">追加</v-btn>
-          </div>
-        </v-form>
-      </v-card>
-
-      <v-card class="places_list">
-        <v-card-title>
-          <span class="card_title">周りの場所</span>
-        </v-card-title>
-        <v-list>
-          <template v-for="(place, index) in places">
-            <v-list-title :key="place.name">
-              <v-list-tile-content class="place_name">
-                <v-list-tile-title>{{ place.name }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-title>
-            <v-divider :key="index"></v-divider>
-          </template>
-        </v-list>
-      </v-card>
+      <add-place-card />
+      <places-list :places="places" />
     </v-flex>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import AddPlaceCard from '@/components/organisms/AddPlaceCard.vue'
+import PlacesList from '@/components/organisms/PlacesList.vue'
 
 interface Place {
   name: string
 }
 
 @Component({
-  components: {},
+  components: { AddPlaceCard, PlacesList },
   layout: 'toppage'
 })
 export default class Index extends Vue {
@@ -60,29 +36,10 @@ export default class Index extends Vue {
   font-size: 3rem;
 }
 
-.white_text {
-  color: white;
-}
-
 .container {
   margin: 0 auto;
   min-height: 100vh;
   text-align: center;
-  .new_place {
-    margin-bottom: 20px;
-    padding: 1rem;
-  }
-  .places_list {
-    padding: 10px;
-    .place_name {
-      font-size: 1.2rem;
-      padding: 10px;
-    }
-  }
-}
-
-.card_title {
-  font-size: 1.5rem;
 }
 
 // Example of Media Query Mixin
