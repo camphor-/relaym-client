6db72464cce3fc97e2b1d03a3547ff7312723482
Arata Sato <ataran@cb93be8c.local>
2019-07-14 14:44:00 +0900

    add add-track logic

--- store/tracklist.ts
+++ store/tracklist.ts
@@ -11,6 +11,9 @@ export const state = () => ({
 export const mutations = {
   setTrackList: (state: State, trackList: Track[]) => {
     state.trackList = trackList
+  },
+  addTrack: (state: State, newTrack: Track) => {
+    state.trackList.push(newTrack)
   }
 }
 
@@ -442,5 +445,59 @@ export const actions = {
       }
     ]
     commit('setTrackList', trackList)
+  },
+  addTrack({ commit }, trackId: string) {
+    const newTrack = getNewTrack(trackId)
+    commit('addTrack', newTrack)
+  }
+}
+
+const getNewTrack = (trackId: string): Track => {
+  return {
+    album: {
+      artists: [
+        {
+          href: 'https://api.spotify.com/v1/artists/0bAsR2unSRpn6BQPEnNlZm',
+          id: '0bAsR2unSRpn6BQPEnNlZm',
+          name: 'Aimer'
+        }
+      ],
+      href: 'https://api.spotify.com/v1/albums/4KiW4nVZ5bUJk2Q8LyXCzs',
+      id: '4KiW4nVZ5bUJk2Q8LyXCzs',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/c0f3fdc9512e35ee56189b4a3a9a23c7c212c353',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/087fe586ca295b1ec8dfa81cd21cff056245458b',
+          width: 300
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/994b94c07eb6b5d475482ebfc16342c539bc80cb',
+          width: 64
+        }
+      ],
+      name: 'BEST SELECTION "noir"'
+    },
+    artists: [
+      {
+        href: 'https://api.spotify.com/v1/artists/0bAsR2unSRpn6BQPEnNlZm',
+        id: '0bAsR2unSRpn6BQPEnNlZm',
+        name: 'Aimer'
+      }
+    ],
+    duration_ms: 328786,
+    href: 'https://api.spotify.com/v1/tracks/6PAVYkfGLNidSQRDZOANCI',
+    id: trackId,
+    name: 'StarRingChild',
+    preview_url:
+      'https://p.scdn.co/mp3-preview/d825a50e933b79a528b8801a82e0ddbc3045bade?cid=774b29d4f13844c495f206cafdad9c86'
   }
 }
