860d29609cb74106ced9ed230eed0f7dc8a3de53
dora <31735614+dora1998@users.noreply.github.com>
2019-07-14 16:33:20 +0900

    PWA Support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,13 +8,65 @@ const nuxtConfig: NuxtConfiguration = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Here Songs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'icons/apple-touch-icon.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: 'icons/apple-touch-icon-57x57.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: 'icons/apple-touch-icon-60x60.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: 'icons/apple-touch-icon-72x72.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: 'icons/apple-touch-icon-76x76.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: 'icons/apple-touch-icon-114x114.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: 'icons/apple-touch-icon-120x120.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: 'icons/apple-touch-icon-144x144.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: 'icons/apple-touch-icon-152x152.png'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'icons/apple-touch-icon-180x180.png'
+      }
+    ]
   },
 
   /*
@@ -85,8 +137,8 @@ const nuxtConfig: NuxtConfiguration = {
   },
 
   manifest: {
-    name: '',
-    short_name: '',
+    name: 'Here Songs',
+    short_name: 'Here Songs',
     description: '',
     start_url: '/',
     display: 'standalone',
