d7db2b57212b0a6a0024faea7c66cfcbbf689a90
Arata Sato <ataran@cb93be8c.local>
2019-07-15 03:09:45 +0900

    Add api util

---
+++ api/main.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8080/api/v1'
+})
+
+export default class {
+  getQueue = async () => {
+    const queue = await instance.get('/queue')
+    return queue
+  }
+
+  addTrack = async (trackId: string) => {
+    const newTrack = await instance.post('/add_track', {
+      uri: `spotify:track:${trackId}`
+    })
+    return newTrack
+  }
+}
