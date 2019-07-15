7fdc44d615c45ad4e8d26f9dfbe1a9d26d4b91f8
dora <31735614+dora1998@users.noreply.github.com>
2019-07-15 13:25:25 +0900

    Add env

--- api/main.ts
+++ api/main.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { GetAvailableDevicesResponse, GetQueueResponse } from '@/api/interfaces'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1'
+  baseURL: process.env.BASE_URL
 })
 
 export default {
