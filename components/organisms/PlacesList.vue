e1ef9349d30a7f999431890697e7d7285249119f
aratasato <14242642+aratasato@users.noreply.github.com>
2019-07-18 15:34:53 +0900

    loginUrlを変数にした

--- components/organisms/PlacesList.vue
+++ components/organisms/PlacesList.vue
@@ -4,11 +4,7 @@
       <span class="card_title">Places near You</span>
     </v-card-title>
     <v-list>
-      <v-list-tile
-        v-for="place in places"
-        :key="place.id"
-        href="https://accounts.spotify.com/authorize?client_id=2d41ee0f36664938a4669dcf5e81a43f&response_type=code&redirect_uri=https%3A%2F%2Fhere-songs.tomoyat1.com%2Fcallback&scope=user-read-playback-state,user-read-currently-playing,user-modify-playback-state,playlist-read-private,playlist-modify-private"
-      >
+      <v-list-tile v-for="place in places" :key="place.id" :href="login_url">
         <v-list-tile-content>
           <v-list-tile-title v-text="place.name"></v-list-tile-title>
           <v-divider :key="place.id"></v-divider>
@@ -26,6 +22,8 @@ import Place from '@/models/Place'
 })
 export default class extends Vue {
   @Prop({ default: [] }) readonly places!: Place[]
+
+  loginUrl = `https://accounts.spotify.com/authorize?client_id=2d41ee0f36664938a4669dcf5e81a43f&response_type=code&redirect_uri=https%3A%2F%2Fhere-songs.tomoyat1.com%2Fcallback&scope=user-read-playback-state,user-read-currently-playing,user-modify-playback-state,playlist-read-private,playlist-modify-private`
 
   @Emit()
   click(id: string) {
