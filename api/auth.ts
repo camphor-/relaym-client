f5fb9ac4d04571a5ad4e85d5cc614b914c66bf5e
dora <31735614+dora1998@users.noreply.github.com>
2019-07-19 23:55:52 +0900

    Change auth flow

---
+++ api/auth.ts
@@ -0,0 +1,23 @@
+export default {
+  isLoggedIn() {
+    // TODO: sessionidなどを使って検証する
+    return false
+  },
+
+  getLoginUrl() {
+    // create login URL
+    const authScopes = [
+      'user-read-playback-state',
+      'user-read-currently-playing',
+      'user-modify-playback-state',
+      'playlist-read-private',
+      'playlist-modify-private'
+    ]
+    const loginUrl = `https://accounts.spotify.com/authorize?client_id=${
+      process.env.CLIENT_ID
+    }&response_type=code&redirect_uri=${encodeURIComponent(
+      process.env.BASE_URL + '/callback'
+    )}&scope=${authScopes.join(',')}`
+    return loginUrl
+  }
+}
