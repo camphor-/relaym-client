b98311e530d1b19ab1f4a759bc300b506ec541b9
aratasato <14242642+aratasato@users.noreply.github.com>
2019-07-20 17:16:49 +0900

    Update playing flag

--- store/tracklist.ts
+++ store/tracklist.ts
@@ -5,13 +5,13 @@ import Device from '@/models/Device'
 interface State {
   trackList: Track[]
   playingTrackId: number
-  playing: boolean
+  pause: boolean
 }
 
 export const state = () => ({
   trackList: [],
   playingTrackId: 4,
-  playing: false
+  pause: true
 })
 
 export const getters = {
@@ -33,8 +33,8 @@ export const mutations = {
   addTrack: (state: State, newTrack: Track) => {
     state.trackList.push(newTrack)
   },
-  setPlayback: (state: State, playing: boolean) => {
-    state.playing = playing
+  setPause: (state: State, pause: boolean) => {
+    state.pause = pause
   },
   nextTrack: (state: State, newTrackId: number) => {
     state.playingTrackId = newTrackId
@@ -50,11 +50,10 @@ export const actions = {
   async addTrack({}, trackURI: string) {
     await Api.addTrack(trackURI)
   },
-  async play({ commit, state }, device: Device) {
+  async play({ state }, device: Device) {
     // TODO: 一時停止にサーバーが対応次第対応する
-    if (state.playing) return
+    if (!state.pause) return
     await Api.play(device.id)
-    commit('setPlayback', true)
   },
   nextSong({ commit }, newTrackId: number) {
     commit('nextSong', newTrackId)
