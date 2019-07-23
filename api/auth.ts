dd423262a545573a202eb6463046badf8212a955
Tomoya Tabuchi <t@tomoyat1.com>
2019-07-23 22:21:51 +0900

    Add user-read-private scope to Spotify OAuth
    * Required to get subscription details.

--- api/auth.ts
+++ api/auth.ts
@@ -9,6 +9,7 @@ export default {
     const authScopes = [
       'user-read-playback-state',
       'user-read-currently-playing',
+      'user-read-private',
       'user-modify-playback-state',
       'playlist-read-private',
       'playlist-modify-private'
