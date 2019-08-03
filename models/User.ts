c0a375f70ee1620be29008714e7b70a448619051
dora <31735614+dora1998@users.noreply.github.com>
2019-08-04 03:35:34 +0900

    Update models

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,12 @@
+/* eslint-disable camelcase */
+import Image from './Image'
+
 interface User {
-  user_id: string
+  id: string
+  uri: string
   display_name: string
+  images: Image[]
+  is_premium: boolean
 }
 
 export default User
