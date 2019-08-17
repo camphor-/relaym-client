19bb774371d1b03a3e42074f05e88aec02d66007
dora <31735614+dora1998@users.noreply.github.com>
2019-08-17 19:15:26 +0900

    ApiException extends Error

--- api/v2/common.ts
+++ api/v2/common.ts
@@ -3,12 +3,37 @@ import axios from 'axios'
 export const instance = axios.create({
   baseURL: process.env.BASE_URL + '/api/v2'
 })
+instance.interceptors.response.use(
+  response => Promise.resolve(response),
+  error => Promise.resolve(error.response)
+)
 
-export class ApiException {
+class BaseError extends Error {
+  constructor(...args) {
+    super(...args)
+
+    // this.name = this.constructor.name; でも問題ないが、
+    // enumerable を false にしたほうがビルトインエラーに近づく、
+    Object.defineProperty(this, 'name', {
+      configurable: true,
+      enumerable: false,
+      value: this.constructor.name,
+      writable: true
+    })
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, BaseError)
+    }
+  }
+}
+
+export class ApiException extends BaseError {
   public readonly statusCode
   public readonly msg
 
   constructor(statusCode, msg) {
+    super({ statusCode, msg })
+
     this.statusCode = statusCode
     this.msg = msg
   }
