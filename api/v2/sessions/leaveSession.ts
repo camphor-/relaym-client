4db5b12d769ed896b585d7d877fecbfda818962e
dora <31735614+dora1998@users.noreply.github.com>
2019-08-17 19:28:25 +0900

    Fix leaveSession

--- api/v2/sessions/leaveSession.ts
+++ api/v2/sessions/leaveSession.ts
@@ -1,7 +1,7 @@
 import { ApiException, instance } from '@/api/v2/common'
 
 const leaveSession = async (id: string) => {
-  const res = await instance.post(`/sessions/${id}/members/`, null, {
+  const res = await instance.delete(`/sessions/${id}/members/`, {
     withCredentials: true
   })
   if (res.status !== 204) {
