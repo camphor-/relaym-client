8d8e61930149930c866d13dfb81da96959766e03
aratasato <14242642+aratasato@users.noreply.github.com>
2019-10-10 21:42:00 +0900

    ログインボタンが実際機能するようにした

--- components/organisms/LoginButton.vue
+++ components/organisms/LoginButton.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="login-button-container">
-    <v-btn round color="white">Login to Spotify</v-btn>
+    <v-btn round color="white" @click="login">Login to Spotify</v-btn>
     <div class="login-description">Spotifyにログインしてセッションを表示</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import ApiV2 from '../../api/v2'
 
 @Component({
   components: {}
 })
-export default class extends Vue {}
+export default class extends Vue {
+  login() {
+    location.href = ApiV2.getLoginUrl()
+  }
+}
 </script>
 
 <style lang="scss" scoped>
