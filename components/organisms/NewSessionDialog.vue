eda58f67d04fcde1ddb06b9810c6891fc40a76a5
dora <31735614+dora1998@users.noreply.github.com>
2019-10-26 23:12:50 +0900

    Use v2 api when create session

--- components/organisms/NewSessionDialog.vue
+++ components/organisms/NewSessionDialog.vue
@@ -11,9 +11,7 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" depressed @click="createNewSession()"
-          >作成
-        </v-btn>
+        <v-btn color="primary" depressed @click="createSession()">作成 </v-btn>
         <v-btn flat color="primary" @click="closeDialog()">
           キャンセル
         </v-btn>
@@ -24,7 +22,6 @@
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
-import ApiV2 from '@/api/v2'
 
 @Component({})
 export default class extends Vue {
@@ -41,13 +38,9 @@ export default class extends Vue {
     this.input(false)
   }
 
-  createNewSession() {
-    // TODO: sessionの作成
-    // const newSession: Session = (await ApiV2.sessions.createSession({
-    //   is_public: this.isPublic
-    // })).session
-    // this.$router.push({ path: `/sessions/${newSession.id}` })
-    location.href = ApiV2.getLoginUrl()
+  @Emit()
+  createSession() {
+    return { isPublic: this.isPublic }
   }
 }
 </script>
