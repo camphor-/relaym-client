b5a218d2f33af5735ab26fa166fbf484ec4b77ac
Arata Sato <densiarata2@gmail.com>
2019-12-17 21:16:43 +0900

    エラーページつくったった (#99)
    * add error page
    
    * change background color

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <toppage-logo />
+    <h1 class="error-page-title">Relaym</h1>
+    <p>{{ error.message }}</p>
+    <div class="home-button-wrapper">
+      <v-btn round color="secondary" class="home" href="/"
+        >Return to Top Page</v-btn
+      >
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import ToppageLogo from '@/components/molecules/ToppageLogo'
+
+@Component({
+  components: {
+    ToppageLogo
+  }
+})
+export default class Error extends Vue {
+  @Prop({ default: {} }) readonly error!: any
+  private message = '404'
+}
+</script>
+
+<style lang="scss" scoped>
+.error-page-title {
+  color: $primary-color;
+}
+
+.home-button-wrapper {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
