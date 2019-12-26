dfb94f8bbeb06be079476eac4b51f1a8fdccfb30
Arata Sato <densiarata2@gmail.com>
2019-12-26 18:33:52 +0900

    再生するときにデバイスが生きているか確認 (#127)
    * セッションに参加していない場合はトップに戻る
    
    * デバイスが見つからないときの処理
    
    * 条件間違っとった
    
    * コントローラーを表示するか
    
    * delete device information

--- components/organisms/BottomController.vue
+++ components/organisms/BottomController.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bottom-controller-wrapper">
-    <div class="bottom-controller elevation-5">
+    <div v-if="showController" class="bottom-controller elevation-5">
       <v-layout align-center justify-space-around>
         <v-btn icon large @click="openDeviceSelectDialog">
           <v-icon>devices</v-icon>
         </v-btn>
-        <v-btn v-if="playable" icon @click="togglePlayback">
+        <v-btn icon @click="togglePlayback">
           <v-icon v-if="paused" color="accent" x-large class="play-icon">
             play_arrow
           </v-icon>
@@ -20,21 +20,28 @@
         </nuxt-link>
       </v-layout>
     </div>
+    <snackbar v-model="showSnackbar" :text="snackbarText" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Emit, Vue } from 'vue-property-decorator'
-import { mapActions, mapGetters, mapState } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import { Playback } from '@/store/currentSession'
+import Track from '@/models/Track'
+import User from '@/models/User'
+import Snackbar from '@/components/molecules/Snackbar.vue'
+import Device from '@/models/Device'
 
 @Component({
+  components: { Snackbar },
   methods: {
-    ...mapActions('currentSession', ['pause', 'play', 'fetchCurrentSession'])
+    ...mapActions('currentSession', ['pause', 'play', 'fetchCurrentSession']),
+    ...mapActions('devices', ['fetchAvailableDevices'])
   },
   computed: {
-    ...mapState('currentSession', ['playback']),
-    ...mapGetters('currentSession', ['playable']),
+    ...mapState('currentSession', ['playback', 'id', 'delegate', 'tracks']),
+    ...mapState('devices', ['availableDevices']),
     paused() {
       return this.playback.paused
     }
@@ -44,17 +51,58 @@ export default class extends Vue {
   private pause!: () => void
   private play!: () => void
   private fetchCurrentSession!: () => void
+  private fetchAvailableDevices!: () => void
 
   private playback!: Playback
-  private playable!: boolean
+
+  private id: string | null
+  private delegate?: User | null
+  private tracks: Track[]
+  private availableDevices: Device[]
+
+  private showSnackbar = false
+  private snackbarText = ''
+  private showController = true
 
   @Emit()
   openDeviceSelectDialog() {}
 
+  playable(): boolean {
+    // セッションに参加していない
+    if (this.id === null) {
+      this.showController = false
+      return false
+    }
+
+    // 再生可能なデバイスがない
+    if (this.availableDevices.length === 0) {
+      this.snackbarText = 'デバイスが見つかりません。'
+      this.showSnackbar = true
+      return false
+    }
+
+    // デバイスを選択する必要がある
+    if (!this.delegate) {
+      this.snackbarText = 'デバイスを選択してください。'
+      this.showSnackbar = true
+      return false
+    }
+
+    // 再生可能な曲があるか確認
+    if (this.playback.head >= this.tracks.length) {
+      this.snackbarText = '曲を追加してください。'
+      this.showSnackbar = true
+      return false
+    }
+
+    return true
+  }
+
   async togglePlayback() {
     await this.fetchCurrentSession()
+    await this.fetchAvailableDevices()
 
-    if (this.playback.paused && this.playable) {
+    if (this.playback.paused && this.playable()) {
       this.play()
     } else {
       this.pause()
