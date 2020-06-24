4851eb3f8525879d53b7f591a07dd67b4e2f04c5
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-24 15:46:30 +0900

    APIエンドポイントを変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -199,9 +199,9 @@ const nuxtConfig: NuxtConfiguration = {
   },
 
   env: {
-    BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
+    BASE_URL: process.env.BASE_URL || 'http://relaym.local:8080',
     BASE_WEBSOCKET_URL:
-      process.env.BASE_WEBSOCKET_URL || 'ws://localhost:8080/api/v2',
+      process.env.BASE_WEBSOCKET_URL || 'ws://relaym.local:8080/api/v3',
     SPOTIFY_CLIENT_ID: '49a1da87167748c6bbe344a15817f184'
   }
 }
