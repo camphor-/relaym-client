39e7e424bfcd649fb5446dd3de122cb926c95f04
dora <31735614+dora1998@users.noreply.github.com>
2020-06-24 17:05:22 +0900

    ログインURLの取得方法を修正

--- api/v3/index.ts
+++ api/v3/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+export const baseURL = process.env.BASE_URL + '/api/v3'
 export const instance = axios.create({
-  baseURL: process.env.BASE_URL + '/api/v3',
+  baseURL,
   withCredentials: true,
   headers: { 'X-CSRF-Token': 'relaym' }
 })
