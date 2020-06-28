91a70d9e3d9722f1a2577c076bc40402b3f286d6
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-28 15:28:38 +0900

    退出・アーカイブ関連の処理をSideMenuから消す

--- components/organisms/SlideMenu.vue
+++ components/organisms/SlideMenu.vue
@@ -13,93 +13,21 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-
-        <v-list-tile @click="exitSession">
-          <v-list-tile-avatar>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>Exit</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile
-          class="bottom-btn"
-          @click="openConfirmTerminateSessionDialog"
-        >
-          <v-list-tile-avatar>
-            <v-icon>archive</v-icon>
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>Terminate and Archive</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
       </v-list>
-      <confirm-terminate-session-dialog
-        v-model="isOpenConfirmTerminateSessionDialog"
-        @on-click-delete="terminateSession"
-      />
     </v-navigation-drawer>
-    <snackbar v-model="showSnackbar" :text="snackbarText" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
-import { mapState } from 'vuex'
-import ConfirmTerminateSessionDialog from '@/components/molecules/ConfirmTerminateSessionDialog.vue'
-import ApiV2 from '@/api/v2'
-import Snackbar from '@/components/molecules/Snackbar.vue'
 
-@Component({
-  components: { ConfirmTerminateSessionDialog, Snackbar },
-  computed: {
-    ...mapState('currentSession', ['id'])
-  }
-})
+@Component({})
 export default class extends Vue {
-  private isOpenConfirmTerminateSessionDialog = false
-  private id!: string | null
-  private showSnackbar = false
-  private snackbarText = ''
-
   @Prop({ default: false }) readonly value!: boolean
 
   @Emit()
   input(isOpen: boolean) {
     return isOpen
-  }
-
-  openConfirmTerminateSessionDialog() {
-    this.isOpenConfirmTerminateSessionDialog = true
-  }
-
-  async terminateSession() {
-    try {
-      await ApiV2.sessions.current.controlPlayback({ state: 'STOP' })
-      this.$router.push('/')
-    } catch (e) {
-      console.error(e)
-      // TODO: エラー処理
-    }
-  }
-
-  async exitSession() {
-    if (this.id) {
-      try {
-        await ApiV2.sessions.leaveSession(this.id)
-        this.showSnackbar = true
-        this.$router.push({ path: '/' })
-        this.snackbarText = 'セッションから退出しました。'
-      } catch (e) {
-        if (e.statusCode === 400) {
-          this.showSnackbar = true
-          this.snackbarText = 'セッションの作成者は退出できません。'
-        } else {
-          console.error(e)
-        }
-      }
-    }
   }
 }
 </script>
@@ -116,10 +44,5 @@ a {
   text-align: center;
   margin: 1rem 0;
   font-size: 2rem;
-}
-.bottom-btn {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
 }
 </style>
