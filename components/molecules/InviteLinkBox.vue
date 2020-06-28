e6ae363a98cdd8f50a19b0ad74ce2cc6f8b34f51
dora <31735614+dora1998@users.noreply.github.com>
2020-06-28 19:29:04 +0900

    シェアボタンのUIを実装

---
+++ components/molecules/InviteLinkBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout class="container">
+    <v-layout row align-center>
+      <v-btn flat icon color="#00B900">
+        <v-icon>fab fa-line</v-icon>
+      </v-btn>
+      <v-btn flat icon>
+        <v-icon>fas fa-share-alt</v-icon>
+      </v-btn>
+      <v-btn depressed>コピー</v-btn>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'InviteLinkBox'
+})
+export default class extends Vue {
+  @Prop({ required: true }) readonly sessionId!: string | null
+
+  get inviteUrl() {
+    if (!this.sessionId) return ''
+    return `${location.origin}/sessions/${this.sessionId}`
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 16px;
+}
+</style>
