8d7c6215788a9bf48ea583ed992e0f970277396f
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-29 00:22:12 +0900

    新しいデバイス取得APIを使うように変更

--- components/organisms/DeviceSelectDialog.vue
+++ components/organisms/DeviceSelectDialog.vue
@@ -23,14 +23,15 @@
 
 <script lang="ts">
 import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator'
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapState } from 'vuex'
 import DeviceList from '@/components/molecules/DeviceList.vue'
-import { Device } from '@/api/v3/types'
+import { Device, Session } from '@/api/v3/types'
 
 @Component({
   components: { DeviceList },
   computed: {
-    ...mapGetters('pages/sessions/detail', ['playableDevices'])
+    ...mapGetters('pages/sessions/detail', ['playableDevices']),
+    ...mapState('pages/sessions/detail', ['session'])
   },
   methods: {
     ...mapActions('pages/sessions/detail', ['fetchAvailableDevices'])
@@ -38,7 +39,8 @@ import { Device } from '@/api/v3/types'
 })
 export default class extends Vue {
   @Prop({ default: false }) readonly value!: boolean
-  private fetchAvailableDevices!: () => void
+  private fetchAvailableDevices!: (sessionId: string) => void
+  private session!: Session | null
 
   @Emit()
   input(isOpen: boolean) {
@@ -47,8 +49,8 @@ export default class extends Vue {
 
   @Watch('value', { immediate: true })
   onValueChange(val: boolean) {
-    if (!val) return
-    this.fetchAvailableDevices()
+    if (!val || !this.session) return
+    this.fetchAvailableDevices(this.session.id)
   }
 
   closeDialog() {
--- store/pages/sessions/detail.ts
+++ store/pages/sessions/detail.ts
@@ -1,7 +1,12 @@
 import { MutationTree, ActionTree } from 'vuex'
 import { Session, SocketMessage, Device } from '@/api/v3/types'
-import { getMyDevices } from '@/api/v3/user'
-import { controlPlayback, getSession, setDevice } from '@/api/v3/session'
+import {
+  getDevices,
+  controlPlayback,
+  getSession,
+  setDevice
+} from '@/api/v3/session'
+
 import { createWebSocket } from '@/api/v3/websocket'
 
 interface State {
@@ -80,8 +85,8 @@ export const actions: ActionTree<State, {}> = {
     commit('setSession', session)
     await dispatch('setProgressTimer')
   },
-  async fetchAvailableDevices({ commit }) {
-    const res = await getMyDevices()
+  async fetchAvailableDevices({ commit }, sessionID: string) {
+    const res = await getDevices(sessionID)
     commit('setAvailableDevices', res)
   },
   async setDevice({ state, dispatch }, deviceId: string) {
