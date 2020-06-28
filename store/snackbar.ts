f3f18775ec666b5a38795d40c9f4af6df78e8cbb
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-06-28 19:28:28 +0900

    Snackbar用のStoreを追加

---
+++ store/snackbar.ts
@@ -0,0 +1,56 @@
+interface State {
+  isOpen: boolean
+  message: string
+  messageType: MessageType
+}
+
+interface SnackbarPayload {
+  message: string
+  messageType: MessageType
+}
+
+export enum MessageType {
+  info,
+  error
+}
+
+export const state = (): State => ({
+  isOpen: false,
+  message: '',
+  messageType: MessageType.info
+})
+
+export const mutations = {
+  open(state: State) {
+    state.isOpen = true
+  },
+  close(state: State) {
+    state.isOpen = false
+  },
+  setMessage(state: State, message: string) {
+    state.message = message
+  },
+  setMessageType(state: State, messageType: MessageType) {
+    state.messageType = messageType
+  }
+}
+
+export const actions = {
+  showSnackbar({ commit, state }, payload: SnackbarPayload) {
+    commit('setMessage', payload.message)
+    commit('setMessageType', payload.messageType)
+
+    // すでに開いている場合は一回とじる
+    if (state.isOpen) {
+      commit('close')
+      setTimeout(() => {
+        commit('open')
+      }, 200)
+      return
+    }
+    commit('open')
+  },
+  closeSnackbar({ commit }) {
+    commit('close')
+  }
+}
