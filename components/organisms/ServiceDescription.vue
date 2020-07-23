397136fd667630ab0dae41dd6b367c0cb980ae0d
dora <31735614+dora1998@users.noreply.github.com>
2020-07-23 17:20:21 +0900

    特徴セクションを仮組み

--- components/organisms/ServiceDescription.vue
+++ components/organisms/ServiceDescription.vue
@@ -6,16 +6,33 @@
         Relaym（リレイム）は、それぞれが聴きたいSpotifyの楽曲を1つのスピーカーで楽しめるWebアプリです。
       </p>
     </section>
+
+    <section>
+      <service-description-header>特徴</service-description-header>
+      <service-feature>
+        <template v-slot:header
+          >Spotify Premiumユーザーが<br />
+          1人いればOK</template
+        >
+        <template v-slot:body
+          ><p>
+            再生する人だけがSpotify
+            Premiumに加入していれば大丈夫。その他の参加者は、登録もログインも一切不要です。
+          </p></template
+        >
+      </service-feature>
+    </section>
   </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import ServiceDescriptionHeader from '@/components/atoms/ServiceDescriptionHeader.vue'
+import ServiceFeature from '@/components/atoms/ServiceFeature.vue'
 
 @Component({
   name: 'ServiceDescription',
-  components: { ServiceDescriptionHeader }
+  components: { ServiceFeature, ServiceDescriptionHeader }
 })
 export default class extends Vue {}
 </script>
