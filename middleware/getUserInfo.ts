c3d4642218fc8acc15e4f47889ae40a0bbb72367
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-08-01 23:25:52 +0900

    既にGetMeを叩いてたら、もう一度叩かないようにする

--- middleware/getUserInfo.ts
+++ middleware/getUserInfo.ts
@@ -2,7 +2,8 @@ import { Context, Middleware } from '@nuxt/types'
 
 const disabledPathes = ['/terms']
 const middleware: Middleware = (ctx: Context) => {
-  if (disabledPathes.includes(ctx.route.path)) {
+  if (disabledPathes.includes(ctx.route.path) || ctx.store.state.user?.me) {
+    return
   }
   ctx.store.dispatch('user/fetchMyUserInfo')
 }
