234434df74d85197dc4fae32e53d910047e1ca7a
dora <31735614+dora1998@users.noreply.github.com>
2020-08-05 03:55:27 +0900

    個別にアイコンをimportするように変更

--- plugins/loadFontAwesome.ts
+++ plugins/loadFontAwesome.ts
@@ -2,14 +2,19 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+  faTwitter,
+  faFacebook,
+  faLine,
+  faGithub
+} from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fab)
+library.add(faTwitter, faFacebook, faLine, faGithub)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
